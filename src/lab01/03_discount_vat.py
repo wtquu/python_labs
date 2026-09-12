00f3bf8901de052193ecad8b3f67048871c45f8b
wtquu <blackbagira2008@gmail.com>
2026-09-12 19:15:41 +0300

    3

--- src/lab01/03_discount_vat.py
+++ src/lab01/03_discount_vat.py
@@ -4,4 +4,6 @@ vat = float(input("НДС: ").replace(",", "."))
 base = price * (1 - discount/100)
 vat_amount = base * (vat/100)
 total = base + vat_amount
-print(f'База после скидки: {base:.2f}\nНДС: {vat_amount:.2f}\nИтого к оплате: {total:.2f}')
+print(f'База после скидки: {base:>10.2f}')
+print(f'НДС:               {vat_amount:>10.2f}')
+print(f'Итого к оплате:    {total:>10.2f}')
