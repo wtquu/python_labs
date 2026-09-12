1d3682ce25392c5e19e726f20dbb90a74431b247
wtquu <blackbagira2008@gmail.com>
2026-09-12 17:45:46 +0300

    3

--- src/lab01/02_sum_avg.py
+++ src/lab01/02_sum_avg.py
@@ -1,3 +1,3 @@
 a = float(input("a: ").replace(",", "."))
 b = float(input("b: ").replace(",", "."))
-print(f'sum={round(a + b, 2):.2f}; avg={round((a + b) / 2, 2):.2f}')
+print(f'sum={a + b:.2f}; avg={(a + b) / 2:.2f}')
